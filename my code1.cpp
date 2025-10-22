8585a9bd102bf9ecdd239f608dcf08d477cf7aef
yosoytayfun <yosoytayfun@gmail.com>
2025-10-21 18:10:30 -0700

    Add initial car registration C++ project
    Add car registration C++ project

---
+++ my code1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+char nombre[15];
+char apellido[15];
+char carro[15];
+char color[15];
+int modelo;
+float longitud;
+
+using namespace std;
+int main() {
+	cout<<"como se llama usted?:" ;
+	cin>>nombre;
+	cout<<"cual es su apellido?:" ;
+	cin>>apellido;
+	cout<<"cual es el carro que desea comprar?:" ;
+	cin>>carro;
+	cout<<"cual es el color del carro?:" ,
+	cin>>color;
+	cout<<"cual es el modelo del carro?:" ,
+	cin>>modelo;
+	cout<<"cual es la longitud del carro?:" ,
+	cin>>longitud;
+
+return 0 ;
+}
+
